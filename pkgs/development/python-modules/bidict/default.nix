df5a255b10bcb674cb6f63aa15917c8fee029057
Florian Koegler <FlorianKoegler@users.noreply.github.com>
2018-03-12 23:56:47 +0100

    add part of dependencies

--- pkgs/development/python-modules/bidict/default.nix
+++ pkgs/development/python-modules/bidict/default.nix
@@ -1,4 +1,7 @@
-{ stdenv, buildPythonPackage, fetchPypi }:
+{ stdenv, lib, buildPythonPackage, fetchPypi
+, setuptools_scm
+, pytestrunner, sortedcontainers, sortedcollections
+}:
 
 buildPythonPackage rec {
   pname = "bidict";
@@ -10,9 +13,19 @@ buildPythonPackage rec {
     sha256 = "08j1pnpzifffxar8s58gs4agwi186qgp4hnwjhqlcr96icr7nn2f";
   };
 
+  buildInputs = [
+    setuptools_scm pytestrunner
+  ];
+
+  checkInputs = [
+    pytestrunner sortedcontainers sortedcollections
+  ];
+
+  doCheck = false;
+
   meta = with stdenv.lib; {
     description = "Efficient, Pythonic bidirectional map implementation and related functionality";
     homepage = "https://github.com/jab/bidict";
-    license = stdenv.lib.licenses.mpl20;
+    license = lib.licenses.mpl20;
   };
 }
